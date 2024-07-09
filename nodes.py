dfabdfce1d088a25fc61f1d61620cc0c901aa28f
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-07-09 19:15:53 +0200

    Small changes in colors and Node code

--- nodes.py
+++ nodes.py
@@ -4,13 +4,14 @@ from pygame import *
 from colors import *
 
 class Node:
-    def __init__(self, x: int, y: int, name: str = "Unnamed", width: int = 60, height: int = 60):
+    def __init__(self, x: int, y: int, name: str = "Unnamed", width: int = 100, height: int = 100):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.name = name
+        self.color = gray
     
     def draw(self, window: pygame.Surface):
-        pygame.draw.rect(window, blue, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height))
     
