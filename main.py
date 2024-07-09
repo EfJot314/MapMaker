81643d9a1e2ac7158e4130f814350adb2daaf362
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-07-09 18:17:20 +0200

    Basic main loop

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ from colors import *
 pygame.init()
 
 
-window = pygame.display.set_mode((600, 500))
+window = pygame.display.set_mode((800, 700))
 pygame.display.set_caption("Map maker")
+
+FPS = 60
+clock = pygame.time.Clock()
 
 #main loop
 while True:
+    #update view
     window.fill(white)
 
     #handle events
@@ -18,6 +22,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit(0)
+
+    #display refresh
+    pygame.display.update()
+    clock.tick(FPS)
     
 
 
